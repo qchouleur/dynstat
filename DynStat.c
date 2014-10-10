a9fbfb29268a98612602b9732d01fdad9d29385f
Quentin Chouleur <qchouleur@pobox.com>
2014-10-10 14:17:57 +0200

    initial valgrind / flawfinder results

--- DynStat.c
+++ DynStat.c
@@ -2,11 +2,15 @@
 /* Objectif : Corriger les erreurs statiques et dynamiques   */
 /*            de ce programme                                */
 /* --------------------------------------------------------- */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[])
 {
 	/* On ne touche pas à la variable "size" */
-	int size = 16;
+    int size = 16;
     int length = 0;
     char * buf;
 	
@@ -14,15 +18,14 @@ int main(int argc, char *argv[])
 	
 	buf = (char*)malloc(sizeof(char)*size + 1);
 	
-	strcpy_s(buf, sizeof(buf), argv[2]);
+	strncpy(buf, argv[2],  sizeof(buf));
 	printf("buffer : %s\n", buf);
 	
 	/* ----------------------------------------------------- */
 	/* On ne touche pas au contenu "testdubuffer"            */
     /* mais on peut créer une variable reprenant ce contenu  */
 	/* ----------------------------------------------------- */
-	
-	strcpy_s(buf, sizeof(buf), "testdubuffer");
+	strncpy(buf, "testdubuffer", sizeof(buf));	
 	
 	length = strlen(buf);
     if(length != 0)
